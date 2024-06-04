2ddf72bfa8a1d435b3fbfe2bd93da660372d0b5b
annieherieau <annie.herieau@gmail.com>
2024-06-04 14:17:18 +0200

    fix user ref in Article + jwt options in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        #  :jwt_authenticatable, jwt_revocation_strategy: Denylist
+         :jwt_authenticatable, jwt_revocation_strategy: Denylist
   
-  # has_many :articles
+  has_many :articles
 end
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,19 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# DEVISE: Config pour utiliser les cookies 
+module DeviseVue
+  class Application < Rails::Application
+
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
+  end
+end
 
 module ThpRailsApi
   class Application < Rails::Application
